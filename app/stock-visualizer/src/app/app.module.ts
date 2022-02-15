f95c63627a222eb6d18219f5962fcb5e5bda2c65
Joshua Park <joshuapark823@gmail.com>
2022-02-14 19:29:28 -0500

    ng-dygraphs

--- app/stock-visualizer/src/app/app.module.ts
+++ app/stock-visualizer/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgDygraphsModule } from 'ng-dygraphs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgDygraphsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
