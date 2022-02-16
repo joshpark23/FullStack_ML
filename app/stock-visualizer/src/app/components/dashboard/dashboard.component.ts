46812c39cc6c7cf0559bbfb100c6cde85f6aae62
Joshua Park <joshuapark823@gmail.com>
2022-02-15 19:11:10 -0500

    area-chart

--- app/stock-visualizer/src/app/components/dashboard/dashboard.component.ts
+++ app/stock-visualizer/src/app/components/dashboard/dashboard.component.ts
@@ -8,32 +8,21 @@ import { Color, ScaleType } from '@swimlane/ngx-charts';
 })
 export class DashboardComponent implements OnInit {
 
-  single = [
-    {
-      "name": "Germany",
-      "value": 8940000
-    },
-    {
-      "name": "USA",
-      "value": 5000000
-    },
-    {
-      "name": "France",
-      "value": 7200000
-    }
-  ];
-  
   multi = [
     {
       "name": "Germany",
       "series": [
         {
+          "name": "1990",
+          "value": 62000000
+        },
+        {
           "name": "2010",
-          "value": 7300000
+          "value": 73000000
         },
         {
           "name": "2011",
-          "value": 8940000
+          "value": 89400000
         }
       ]
     },
@@ -42,12 +31,16 @@ export class DashboardComponent implements OnInit {
       "name": "USA",
       "series": [
         {
+          "name": "1990",
+          "value": 250000000
+        },
+        {
           "name": "2010",
-          "value": 7870000
+          "value": 309000000
         },
         {
           "name": "2011",
-          "value": 8270000
+          "value": 311000000
         }
       ]
     },
@@ -56,31 +49,50 @@ export class DashboardComponent implements OnInit {
       "name": "France",
       "series": [
         {
+          "name": "1990",
+          "value": 58000000
+        },
+        {
           "name": "2010",
-          "value": 5000002
+          "value": 50000020
         },
         {
           "name": "2011",
-          "value": 5800000
+          "value": 58000000
+        }
+      ]
+    },
+    {
+      "name": "UK",
+      "series": [
+        {
+          "name": "1990",
+          "value": 57000000
+        },
+        {
+          "name": "2010",
+          "value": 62000000
         }
       ]
     }
   ];
+  
 
   view: [number, number] = [800, 800];
 
-  // options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Country';
-  showYAxisLabel = true;
-  yAxisLabel = 'Population';
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
 
   colorScheme: Color = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5'],
     name: 'Joshua',
     selectable: false,
     group: ScaleType.Time
